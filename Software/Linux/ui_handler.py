c65d66600fc6f7298296576276b0e041a2d2b48d
ACloakedFly <dominiquenegm@outlook.com>
2026-03-15 22:21:42 -0400

    Wait do I even need to join every thread?

--- Software/Linux/ui_handler.py
+++ Software/Linux/ui_handler.py
@@ -19,6 +19,8 @@ along with Thoth's Oracle; if not, see <https://www.gnu.org/licenses/>
 ===========================================================================
 */
 // ui_handler.py """
+import sys
+
 import pystray
 from pystray import Menu, MenuItem
 from PIL import Image
@@ -54,8 +56,9 @@ def on_exit(icon, query):
     global notifying
     print("Exitting")
     notifying = False
-    main_exit()
     icon.stop()
+    sys.exit()
+    #main_exit()
 
 def on_shortcut():
     subprocess.Popen(["xdg-open", os.curdir])
